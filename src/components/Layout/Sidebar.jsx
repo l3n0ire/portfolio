b2bc485c9a4e692bc8fa78746931a3a90d5b76da
Colin Lin <colincool100@gmail.com>
2020-05-19 16:44:56 -0400

    fixed mobile navigation

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Sidebar.css';
 import {Link} from 'react-router-dom';
 import resume from '../pages/images/RESUME.pdf'
 
 
-
 export default function Sidebar(props) {
+  const [isChecked,setChecked] =useState(false,false);
+
     return (     
       <div style={{'display': props.display}}>
         <header style={{'background': props.colour}}>
@@ -13,15 +14,15 @@ export default function Sidebar(props) {
         </header>  
         <div className="menu-wrap" >
           <label style={{fontSize:0}} htmlFor='dummy'>a</label>
-          <input id='dummy' type="checkbox" className="toggler"/>
+          <input id='dummy' type="checkbox" className="toggler" onClick={()=>setChecked(!isChecked)} checked={isChecked} onChange={()=>console.log("clicked")}/>
             <div className="hamburger"><div></div></div>
               <div className="menu">
                 <div>
                   <div>
                     <ul>
-                    <li><Link className="link-style" to='/'>Home</Link></li>
-                    <li><a rel="noopener noreferrer" className="link-style" href={resume} target='_blank'>Resume</a></li>
-                    <li><Link className="link-style" to='/contact'>Contact</Link></li>
+                    <li><Link onClick={()=>setChecked(!isChecked)} className="link-style" to='/'>Home</Link></li>
+                    <li><a onClick={()=>setChecked(!isChecked)} rel="noopener noreferrer" className="link-style" href={resume} target='_blank'>Resume</a></li>
+                    <li><Link onClick={()=>setChecked(!isChecked)} className="link-style" to='/contact'>Contact</Link></li>
                     </ul>
                   </div>
                 </div>
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,7 +3,7 @@ import {Fragment} from 'react';
 import './styles/Contact.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faLinkedin,faGithub } from '@fortawesome/free-brands-svg-icons'
-import{faIdCard, faPhone, faMapMarkerAlt, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import{faIdCard, faPhone, faMapMarkerAlt, faEnvelope, faShareAltSquare} from '@fortawesome/free-solid-svg-icons'
 
 
 function Contact() {
@@ -21,25 +21,28 @@ function Contact() {
                     <div className="aboutBody">
                         <div className='contactRow'>
                             <FontAwesomeIcon color='#7b82de' className="contactIcon"  icon={faIdCard} size='3x' />
-                            <h4 className="contactText" >Colin Lin</h4>
+                            <p className="contactText" >Colin Lin</p>
                         </div>
                         <div className='contactRow'>
                             <FontAwesomeIcon color='#7b82de' className="contactIcon" icon={faPhone} flip="horizontal" size='3x' />
-                            <h4 className="contactText" >647-470-9183</h4>
+                            <p className="contactText" >647-470-9183</p>
                         </div>
                         <div className='contactRow'>
                             <FontAwesomeIcon color='#7b82de' className="contactIcon" icon={faMapMarkerAlt} size='3x' />
-                            <h4 className="contactText" >125 Highhill Drive, Toronto, Ontario M1T 1N8 Canada</h4>
+                            <p className="contactText" >125 Highhill Drive, Toronto, Ontario M1T 1N8 Canada</p>
                         </div>
                         <div className='contactRow'>
                             <FontAwesomeIcon color='#7b82de' className="contactIcon" icon={faEnvelope} size='3x' />
-                            <h4 className="contactText" >colin.lin@mail.utoronto.ca</h4>
+                            <p className="contactText" >colin.lin@mail.utoronto.ca</p>
                         </div>
-                            <div className="profileIcons" >
+                        <div className='contactRow'>
+                            <FontAwesomeIcon color='#7b82de' className="contactIcon" icon={faShareAltSquare} size='3x' />
+                            <div className="profileIcons contactText" style={{padding:0}} >
                                 <a rel="noopener noreferrer" target='_blank' href="https://www.facebook.com/colincool100"> <FontAwesomeIcon color= '#3b5998' icon={faFacebook} size='3x'/></a>
                                 <a rel="noopener noreferrer" target='_blank' href="https://www.linkedin.com/in/colin-lin-2001/"><FontAwesomeIcon color='#2867B2' icon={faLinkedin} size='3x'/></a> 
                                 <a rel="noopener noreferrer" target='_blank' href="https://github.com/l3n0ire"><FontAwesomeIcon icon={faGithub} size='3x' /></a>
                             </div>
+                        </div>
                 </div>
 
 
