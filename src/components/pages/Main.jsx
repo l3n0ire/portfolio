d540e066dc589f9f71d9aa8eba3ccede5c0e6bc0
Colin Lin <colincool100@gmail.com>
2020-09-20 17:53:24 -0400

    updated projects and skills

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -6,6 +6,10 @@ import courierU from './images/courierU.png'
 import successStories from './images/successStories.png'
 import tftutsc from './images/tftutsc.png'
 import timetableBuilder from './images/ttb.png'
+import physicsPlaygroundU from './images/physicsPlaygroundU.png'
+import manageU from './images/manageU.png'
+import recycleRush from './images/recycleRush.png'
+import passwordManager from './images/passwordManager.png'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -32,7 +36,7 @@ class Index extends Component{
                 <div className="profileCardBody">
                 <h1>Colin Lin</h1>
                 <p>Software Developer</p>
-                <p>Co-op Computer Science University of Toronto</p>
+                <p>Computer Science Co-op University of Toronto</p>
                 <div className="profileIcons">
     <a rel="noopener noreferrer" target='_blank' href="https://www.facebook.com/colincool100"> <FontAwesomeIcon color= '#3b5998' icon={faFacebook} size='2x'/></a>
     <a rel="noopener noreferrer" target='_blank' href="https://www.linkedin.com/in/colin-lin-2001/"><FontAwesomeIcon color='#2867B2' icon={faLinkedin} size='2x'/></a> 
@@ -62,6 +66,13 @@ class Index extends Component{
                 <p>Hi, my name is Colin. I'm an aspiring software developer with experience in both frontend UX design and backend API development. 
                    I describe myself as someone who is ambitious, a quick learner, and has a passion for developing software with a positive social impact.
                 </p>
+                <h4 style={{display:"inline"}}>Languages: </h4>
+                <p style={{display:"inline"}}>Java, Python, C, C#, Bash, HTML, CSS, JavaScript</p>
+                <br></br>
+                <h4 style={{display:"inline"}}>Tools and technologies: </h4>
+                <p style={{display:"inline"}}>NodeJS, React, Git, MongoDB, jQuery, Jira, Bootstrap</p>
+
+
                 </div>
                 
                 <div className="aboutBody">
@@ -82,9 +93,11 @@ class Index extends Component{
                             <div className='aboutBodyBoxHead'>
                                 <FontAwesomeIcon color='#7b82de' icon={faCoffee} size='3x' />
                                 <div className='aboutBodyBoxText'>
-                                    <h4>API Integration</h4>
-                                    <p>Familiar with integrating REStful web services and APIs such as MapBox API and Google Cloud
-                                        with frontend applications 
+                                    <h4>Object Oriented Programming</h4>
+                                    <p>
+                                        Strong knowledge OOP concepts such as Polymorphism as well as design patterns
+                                        such as Factory Methods and Singleton.
+                
                                     </p>
                                 </div>
                             </div>
@@ -172,6 +185,77 @@ class Index extends Component{
                         </div>
                     </div>
                     <div className="box">
+                        <img src={manageU} alt="oops"/>
+                        <div className="box-text">
+                            <h1>ManageU</h1>
+                            <div className='tags'>
+                                <a href='/' className='btn btn-light'>Python</a>
+                                <a href='/' className='btn btn-light'>Heroku</a>
+                                <a href='/' className='btn btn-light'>discord.py</a>
+                            </div>
+                            <p>
+                            A discord bot that helps university students keep track of important dates for their courses
+                            </p>
+                            <div className ='btnPair'>
+                                <a rel="noopener noreferrer" target='_blank' href='https://github.com/l3n0ire/pythonDiscordBot' className ='btn btn-secondary'>View Code</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img src={physicsPlaygroundU} alt="oops"/>
+                        <div className="box-text">
+                            <h1>Physics Playground</h1>
+                            <div className='tags'>
+                                <a href='/' className='btn btn-light'>C#</a>
+                                <a href='/' className='btn btn-light'>Unity</a>
+                                <a href='/' className='btn btn-light'>ReactJS</a>
+                            </div>
+                            <p>
+                                Virtual physics lab simulator designed to help students learn remotely
+                            </p>
+                            <div className ='btnPair'>
+                                <a rel="noopener noreferrer" target='_blank' href='https://physicsplaygroundu.netlify.app/' className="btn btn-dark">View Demo</a>
+                                <a rel="noopener noreferrer" target='_blank' href='https://github.com/l3n0ire/PhysicsPlaygroundUnity' className ='btn btn-secondary'>View Code</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img src={recycleRush} alt="oops"/>
+                        <div className="box-text">
+                            <h1>Recycle Rush</h1>
+                            <div className='tags'>
+                                <a href='/' className='btn btn-light'>C#</a>
+                                <a href='/' className='btn btn-light'>Unity</a>
+                                <a href='/' className='btn btn-light'>Android Studio</a>
+                                <a href='/' className='btn btn-light'>Java</a>
+                            </div>
+                            <p>
+                            Non-Profit mobile game aimed to encourage recycling among youth
+                            </p>
+                            <div className ='btnPair'>
+                                <a rel="noopener noreferrer" target='_blank' href='https://play.google.com/store/apps/details?id=com.ColinLin.RecycleRush&hl=en' className="btn btn-dark">Download</a>
+                                <a rel="noopener noreferrer" target='_blank' href='https://github.com/l3n0ire/RecycleRushAndroid' className ='btn btn-secondary'>View Code</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img src={passwordManager} alt="oops"/>
+                        <div className="box-text">
+                            <h1>Password Manager</h1>
+                            <div className='tags'>
+                                <a href='/' className='btn btn-light'>Java</a>
+                                <a href='/' className='btn btn-light'>Swing</a>
+                                <a href='/' className='btn btn-light'>OOP</a>
+                            </div>
+                            <p>
+                                Lightweight, cross-platform password manager made in java
+                            </p>
+                            <div className ='btnPair'>
+                                <a rel="noopener noreferrer" target='_blank' href='https://github.com/l3n0ire/PasswordManager' className ='btn btn-secondary'>View Code</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box">
                         <img src={tftutsc} alt="oops"/>
                         <div className="box-text">
                             <h1>Teamfight UTSC</h1>
@@ -207,6 +291,7 @@ class Index extends Component{
                             </div>
                         </div>
                     </div>
+                    
             </div>
 
         </div>
